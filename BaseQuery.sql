3b86b3ac8228202ec7d79ecc336aae171b9ee1f9
whiteuserface92 <white.user.face@gmail.com>
2025-01-08 15:19:30 +0900

    database query update commit

--- BaseQuery.sql
+++ BaseQuery.sql
@@ -1,3 +1,34 @@
+-- test.authority definition
+
+CREATE TABLE `authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authority` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKr1wgeo077ok1nr1shx0t70tg8` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.hibernate_sequence definition
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.message definition
+
+CREATE TABLE `message` (
+  `subject` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fromName` varchar(255) NOT NULL,
+  `data` varchar(255) NOT NULL,
+  `toName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 -- test.`user` definition
 
 CREATE TABLE `user` (
@@ -6,4 +37,51 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `enabled` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.conversations definition
+
+CREATE TABLE `conversations` (
+  `conversation_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`conversation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.participants definition
+
+CREATE TABLE `participants` (
+  `participant_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `conversation_id` bigint(20) DEFAULT NULL,
+  `joined_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`participant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.messages definition
+
+CREATE TABLE `messages` (
+  `message_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `conversation_id` bigint(20) NOT NULL,
+  `sender_id` bigint(20) NOT NULL,
+  `content` varchar(455) DEFAULT NULL,
+  `sent_at` datetime NOT NULL,
+  `read_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- test.attachments definition
+
+CREATE TABLE `attachments` (
+  `attachment_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `message_id` bigint(20) NOT NULL,
+  `file_url` varchar(255) NOT NULL,
+  `file_type` varchar(50) NOT NULL,
+  `uploaded_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`attachment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
